b6471bc11011aa1997f7755891314bbd1c174510
David Tran <dv.tran99@gmail.com>
2019-10-26 17:52:46 -0400

    Added some registers and signals

--- axa.v
+++ axa.v
@@ -1,5 +1,7 @@
 //Based on Dietz Solution for assembler
 `define WORD[15:0] //size of instruciton
+`define DATA[15:0] //size of data
+`define ADDR[15:0] //size of address
 
 //Bits represented with instruciton
 `define OP[15:10] //6 bits for Op code
@@ -14,6 +16,8 @@
 `define STATE[6:0]
 `define REGSIZE [15:0] //According to AXA
 `define MEMSIZE [65536:0]
+`define USIZE [15:0] //Undo stack size
+`define INDEX [3:0] //Undo stack index
 
 //Define OPcodes
 `define OPadd  6'b000010
@@ -33,7 +37,7 @@
 `define OPor   6'b010010
 `define OPand  6'b010011
 `define OPdup  6'b010100
-`define OPfail 6'001111
+`define OPfail 6'b001111
 `define OPxhi  6'b100001
 `define OPxlo  6'b101001
 `define OPlhi  6'b110001
@@ -43,14 +47,38 @@
 `define OPjerr 6'b001110
 `define OPland 6'b010000
 `define OPcom  6'b000001
+`define OPsys  6'b000000
 
-`define NOP    16'b0
-
+`define NOP    16'hffff
 
 module processor(halt, reset, clk);
 output reg halt;
-inputr reset, clk;
+input reset, clk;
 
+reg `DATA regfile `REGSIZE; //Register file
+reg `DATA datamem `MEMSIZE; //Data memory
+reg `WORD instmem `MEMSIZE; //Instruction memory
+reg `ADDR pc; //Program Counter
+reg `ADDR targetpc, landpc, lc; //Target PC, Landing PC, Current PC
+reg `STATE s; //State
+reg `DATA ustack `USIZE; //Undo stack
+reg `INDEX usp; //Undo stack pointer
+reg `WORD ir; //Instruction Register
+
+reg `WORD ir0, ir1, ir2; //Pipeline IR
+reg `WORD d1, d2; //Pipeline destination register
+reg `WORD s1, s2; //Pipeline source
+
+wire pendjb; //Check for jump/branch
+wire jb; //Is it a jump or branch?
+wire zero, nzero, neg, nneg; //Checks jump/branch conditions
+
+assign pendjb = (ir2 `OP == `OPjz) || (ir2 `OP == `OPjnz) || (ir2 `OP == `OPjn) || (ir2 `OP == `OPjnn);
+assign jb = (ir2 `INSSET == 1); //1 if branch, 0 if jump
+assign zero = (d2 == 0);
+assign nzero = (d2 != 0);
+assign neg = (d2 < 0);
+assign nneg = (d2 >= 0);
 
 //reset
 always @(reset) begin
@@ -81,7 +109,7 @@ wire halted;
 processor PE(halted, reset, clk);
 initial begin
   $dumpfile;
-  $dumpvars(0, PE, PE.regfile[1], PE.regfile[2], PE.mainmem[4]);
+  $dumpvars(0, PE);
   #10 reset = 1;
   #10 reset = 0;
   while (!halted) begin
